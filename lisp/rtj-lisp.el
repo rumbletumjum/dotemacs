98e819461d271fe2f0e1b184015aa85cbf129fa7
Ron <rumbletumjum@gmail.com>
2018-04-01 19:44:10 -0400

    make slime aware of clisp + sbcl

--- lisp/rtj-lisp.el
+++ lisp/rtj-lisp.el
@@ -1,4 +1,10 @@
 (use-package slime
-  :ensure t)
+  :ensure t
+  :config
+  ;; (setq inferior-lisp-program "/usr/local/bin/sbcl")
+  (setq slime-lisp-implementations
+        '((sbcl ("sbcl"))
+          (clisp ("clisp"))))
+  (setq slime-contribs '(slime-fancy)))
 
 (provide 'rtj-lisp)
