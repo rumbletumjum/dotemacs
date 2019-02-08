fc1f347c04649fd0f0a553a2381239016e52277c
rumbletumjum <rumbletumjum@gmail.com>
2019-02-08 13:31:04 -0500

    Clean up Scala config

--- lisp/rtj-scala.el
+++ lisp/rtj-scala.el
@@ -1,12 +1,14 @@
 (use-package scala-mode
   :ensure t
+  :pin melpa
   :commands scala-mode)
 
-(use-package sbt-mode
-  :ensure t
-  :pin melpa)
-
 (use-package ensime
+  :ensure t
+  :pin melpa-stable
+  :hook (scala-mode . ensime))
+
+(use-package sbt-mode
   :ensure t
   :pin melpa)
 
