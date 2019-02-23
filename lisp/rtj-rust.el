b048f452f882bf70eb628268e3a46df4e2dadf5f
rumbletumjum <rumbletumjum@gmail.com>
2019-02-22 22:14:34 -0500

    [Rust] add company configuration

--- lisp/rtj-rust.el
+++ lisp/rtj-rust.el
@@ -5,6 +5,8 @@
 (use-package racer
   :ensure t
   :hook ((rust-mode . racer-mode))
+  :bind (:map rust-mode-map
+              ("TAB" . 'company-indent-or-complete-common))
   :config
   (setq racer-rust-src-path
         "/Users/ron/.rustup/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src")
