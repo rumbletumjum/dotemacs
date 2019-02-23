90174e41f78a2b32087a0ee2fc67dbf99c532e6b
rumbletumjum <rumbletumjum@gmail.com>
2019-02-22 22:02:17 -0500

    [Rust] add racer-mode

--- lisp/rtj-rust.el
+++ lisp/rtj-rust.el
@@ -2,4 +2,12 @@
   :ensure t
   :commands rust-mode)
 
+(use-package racer
+  :ensure t
+  :hook ((rust-mode . racer-mode)
+         (racer-mode. eldoc-mode))
+  :config
+  (setq racer-rust-src-path
+        "/Users/ron/.rustup/toolchains/stable-x86_64-apple-darwin/lib/rustlib/src/rust/src"))
+
 (provide 'rtj-rust)
