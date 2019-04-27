e3014a50a86351465c7f14d382936bbb5d15b3c8
rumbletumjum <rumbletumjum@gmail.com>
2019-04-27 10:49:47 -0400

    macrostep + ocaml-user-setup

--- init.el
+++ init.el
@@ -415,3 +415,19 @@
 
 (use-package color-identifiers-mode
   :ensure t)
+
+(define-key emacs-lisp-mode-map (kbd "C-c e") 'macrostep-expand)
+
+;; see https://github.com/joddie/macrostep/issues/11
+(defun my-macrostep-expand-wrapper ()
+  "Workaround `macrostep-expand' not liking white-space after a sexp."
+  (interactive)
+  (when (and (= ?\n (char-after))
+             (= (point) (cdr (bounds-of-thing-at-point 'sexp))))
+    (backward-char))
+  (macrostep-expand))
+
+(define-key emacs-lisp-mode-map (kbd "C-c e") 'my-macrostep-expand-wrapper)
+;; ## added by OPAM user-setup for emacs / base ## 56ab50dc8996d2bb95e7856a6eddb17b ## you can edit, but keep this line
+(require 'opam-user-setup "~/.emacs.d/opam-user-setup.el")
+;; ## end of OPAM user-setup addition for emacs / base ## keep this line
