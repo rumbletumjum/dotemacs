79eda66edbbf62f693c6023162104f1bcff941c7
rumbletumjum <rumbletumjum@gmail.com>
2019-05-14 15:33:47 -0400

    additional company mode configuration

--- lisp/rtj-company.el
+++ lisp/rtj-company.el
@@ -1,6 +1,11 @@
 (use-package company
   :ensure t
   :defer 5
-  :init (global-company-mode))
+  :config
+  (setq company-idle-delay 0.1
+        company-global-modes '(not org-mode)
+        company-minimum-prefix-length 1)
+  (add-hook 'after-init-hook 'global-company-mode))
+
 
 (provide 'rtj-company)
